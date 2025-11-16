c994328393d6dad74cd0eeb00a2705fa85b5f3cd
DenisMihailow <barakata8@gmail.com>
2025-11-16 23:04:58 +0200

    Update UserList.jsx

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -1,13 +1,14 @@
 import { useEffect, useState } from "react";
-import UserItem from "./UserItem";
+import UserItem from "./UserItem.jsx";
 
-export default function UserList(){
+export default function UserList() {
   const [users, setUsers] = useState([]);
-  useEffect(()=>{
+
+  useEffect(() => {
         fetch('http:/localhost:3030/jsonstore/users')
-        .then(result =>{
-          setUsers(Object.values(result));
-          
+        .then(response => response.json())
+        .then(result => {
+          setUsers(Object.values(result));          
         })
         .catch((err)=> alert(err.message));
   },[]);
@@ -15,10 +16,61 @@ export default function UserList(){
         <div className="table-wrapper">
         <table className="table">
           <thead>
-            
+            <tr>
+              <th>
+                Image
+              </th>
+              <th>
+                First name<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-down"
+                  className="icon svg-inline--fa fa-arrow-down Table_icon__+HHgn" role="img"
+                  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                  <path fill="currentColor"
+                    d="M374.6 310.6l-160 160C208.4 476.9 200.2 480 192 480s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 370.8V64c0-17.69 14.33-31.1 31.1-31.1S224 46.31 224 64v306.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0S387.1 298.1 374.6 310.6z">
+                  </path>
+                </svg>
+              </th>
+              <th>
+                Last name<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-down"
+                  className="icon svg-inline--fa fa-arrow-down Table_icon__+HHgn" role="img"
+                  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                  <path fill="currentColor"
+                    d="M374.6 310.6l-160 160C208.4 476.9 200.2 480 192 480s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 370.8V64c0-17.69 14.33-31.1 31.1-31.1S224 46.31 224 64v306.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0S387.1 298.1 374.6 310.6z">
+                  </path>
+                </svg>
+              </th>
+              <th>
+                Email<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-down"
+                  className="icon svg-inline--fa fa-arrow-down Table_icon__+HHgn" role="img" xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 384 512">
+                  <path fill="currentColor"
+                    d="M374.6 310.6l-160 160C208.4 476.9 200.2 480 192 480s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 370.8V64c0-17.69 14.33-31.1 31.1-31.1S224 46.31 224 64v306.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0S387.1 298.1 374.6 310.6z">
+                  </path>
+                </svg>
+              </th>
+              <th>
+                Phone<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-down"
+                  className="icon svg-inline--fa fa-arrow-down Table_icon__+HHgn" role="img"
+                  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                  <path fill="currentColor"
+                    d="M374.6 310.6l-160 160C208.4 476.9 200.2 480 192 480s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 370.8V64c0-17.69 14.33-31.1 31.1-31.1S224 46.31 224 64v306.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0S387.1 298.1 374.6 310.6z">
+                  </path>
+                </svg>
+              </th>
+              <th>
+                Created
+                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-down"
+                  className="icon active-icon svg-inline--fa fa-arrow-down Table_icon__+HHgn" role="img"
+                  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                  <path fill="currentColor"
+                    d="M374.6 310.6l-160 160C208.4 476.9 200.2 480 192 480s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 370.8V64c0-17.69 14.33-31.1 31.1-31.1S224 46.31 224 64v306.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0S387.1 298.1 374.6 310.6z">
+                  </path>
+                </svg>
+              </th>
+              <th>Actions</th>
+            </tr>
           </thead>
           <tbody>
-            <UserItem />
+            {users.map(user => <UserItem  key={user._id} />)}
           </tbody>
         </table>
         <button className="btn-add btn">Add new user</button>
